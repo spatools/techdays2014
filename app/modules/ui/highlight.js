d8ed2c9c06dbd7c821e3a69645d56e66c5bcae11
Maxime LUCE <contact@somatic.fr>
2014-02-10 00:10:28 +0100

    Append a Prism Highlight BindingHandler

---
+++ app/modules/ui/highlight.js
@@ -0,0 +1,12 @@
+﻿define(["knockout", "prism", "css!prism"], function (ko, Prism) {
+    ko.bindingHandlers.highlight = {
+        update: function (element, valueAccessor) {
+            var value = ko.unwrap(valueAccessor());
+
+            if (typeof value === "string")
+                ko.bindingHandlers.html.update(element, valueAccessor);
+
+            Prism.highlightElement(element);
+        }
+    };
+});
