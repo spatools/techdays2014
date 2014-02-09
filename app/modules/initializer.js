985161f386f491f34e45e769b0f80543c4f1ee61
Maxime LUCE <contact@somatic.fr>
2014-02-10 00:11:25 +0100

    Append an initializer module to manage app startup

---
+++ app/modules/initializer.js
@@ -0,0 +1,19 @@
+﻿define(["promise"], function (Promise) {
+    
+    function base() {
+        return new Promise(function (resolve) {
+            require(
+                ["modules/ui"],
+                function () { resolve(); }
+            );
+        });
+    }
+
+    function initialize() {
+        return base();
+    }
+
+    return {
+        initialize: initialize
+    };
+});
