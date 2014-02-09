1649d5d47ba748f195d69e0f957479fe61c94f31
Maxime LUCE <contact@somatic.fr>
2014-02-10 00:29:25 +0100

    Append demo samples

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -9,8 +9,11 @@
         activate: function () {
             router.map([
                 { route: '', moduleId: 'viewmodels/home', title: "Home", nav: true },
-                {"route":"about","moduleId":"viewmodels/about","title":"About","nav":true},
+                {"route":"patterns/promise","moduleId":"viewmodels/patterns/promise","title":"Promise","nav":true},
+                {"route":"patterns/memoization","moduleId":"viewmodels/patterns/memoization","title":"Memoization","nav":true},
+                {"route":"patterns/reuse","moduleId":"viewmodels/patterns/reuse","title":"Reuse","nav":true},
                 /*{durandal:routes}*/
+                {"route":"about","moduleId":"viewmodels/about","title":"About","nav":true},
             ]).buildNavigationModel();
             
             return router.activate();
