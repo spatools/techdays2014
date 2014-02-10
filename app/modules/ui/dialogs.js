e2a727e545eba9b1c93e17771c07168dd03b5319
Maxime LUCE <contact@somatic.fr>
2014-02-10 01:20:19 +0100

    Append a cordova notifications implementation

--- app/modules/ui/dialogs.js
+++ app/modules/ui/dialogs.js
@@ -1,4 +1,21 @@
 ﻿define(["knockout", "promise", "durandal/app"], function (ko, Promise, app) {
+    if (CORDOVA) {
+        app.showMessage = function (message, title, options) {
+            return new Promise(function (resolve) {
+                var boxOptions = options || ['Ok'];
+
+                if (boxOptions.length === 1) {
+                    title = title || "Information";
+                    navigator.notification.alert(message, function () { resolve(options[0]); }, title, boxOptions[0]);
+                }
+                else {
+                    title = title || "Confirmation";
+                    navigator.notification.confirm(message, function (i) { resolve(options[i - 1]); }, title, boxOptions);
+                }
+            });
+        };
+    }
+
     app.showChoice = function (message, title) {
         return app.showMessage(message, title, ["Oui", "Non", "Annuler"]).then(function (result) {
             if (result === "Annuler")
